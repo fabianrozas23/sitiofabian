7a5b2397386faafb8e1db8af8a20f5389a7c6c48
fabian <fabian.rozas@alumnos.uv.cl>
2020-12-09 18:53:49 -0300

    actualización validador

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,12 @@ function validate() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>INGRESE SU RUT</p>
+          <p><h2>INGRESE SU RUT</h2></p>
+          <p>Este validador es para pobrar si su rut es verificable. Debe ingresar el rut sin puntos y con guión. Luego
+            se debe ingresar la contraseña (password) para finalizar con el validador.
+          </p>
           <Validador placeholder="00000000-0" />
+          <p><h3>Muchas Gracias por su participación!</h3></p>
         </header>
       </div>
     );
